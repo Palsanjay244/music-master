3d35c1f4a41aa1e735b44dcfcc3896c1a9ff863d
Pal Sanjay <palsanjay244@gmail.com>
2019-07-21 21:50:42 +0530

    Enter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
+
+class App extends Component {
+
+  state = {artistQuery : '',artist : null, tracks : [] };
+
+  updateQuery = event => {
+    this.setState({artistQuery:event.target.value});
+  }
+
+  searchArtist=()=>{
+   fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+   .then(response=>response.json())
+   .then(json=>{
+     if (json.artists.total > 0) {
+       const artist = json.artists.items[0];
+       this.setState({artist});
+
+       fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+         .then(response => response.json())
+         .then(json =>this.setState({tracks:json.tracks}))
+         .catch(error => alert(error.message));
+   }
+  })
+  .catch(error=>alert(error.message));
+  }
+
+  keyPress=event=>{
+   if(event.key === 'Enter')
+   {
+      this.searchArtist();
+   }
+  }
+
+  render() {
+    console.log('this.state',this.state);
+    return (
+      <div>
+        <h3>Music Master</h3>
+        <input placeholder='Search'
+               onChange={this.updateQuery}
+               onKeyPress={this.keyPress}
+        />
+        <button onClick={this.searchArtist}
+                variant = "dark" 
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Search</button>
+      </div>
+    );
+  }
 }
 
 export default App;
